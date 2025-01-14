fef7693b4444b4f21d1f992f5893756a23797a57
OsamaBangash8840 <osama.khan8840@gmail.com>
2025-01-14 15:43:59 +0500

    cookie modifications

--- models/User.js
+++ models/User.js
@@ -1,31 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const regSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
-        min:3,
-        max:100,
-        required:true,
+        min: 3,
+        max: 100,
+        required: true,
     },
-    email:{
-        type:String,
-        min:3,
-        max:100,
-        unique:true,
-        required:true
+    email: {
+        type: String,
+        min: 3,
+        max: 100,
+        unique: true,
+        required: true,
     },
-    password:{
-        type:String,
-        min:8,
-        max:30,
-        required:true
+    password: {
+        type: String,
+        min: 8,
+        max: 30,
     },
-    resetPasswordToken:String,
-    resetPasswordExpires:Date,
-    date:{
-        type:Date,
-        default:Date.now
-    }
-})
+    googleId: {
+        type: String,
+        unique: true,
+        sparse: true, // Allows for unique but optional fields
+    },
+    avatar: {
+        type: String,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-module.exports = mongoose.model("User",regSchema);
+module.exports = mongoose.model("User", regSchema);
